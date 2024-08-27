7ebcedcbe9122d5e2621893c4cf271113db1ae57
Kishmapreethi <kishma04@gmail.com>
2024-08-27 11:40:40 +0530

    uipages

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './DescriptionBox.css';
+
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade"> Reviews(122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>E-commerce is a popular term for electronic commerce or even internet commerce. The name is self-explanatory, it is the meeting of buyers and sellers on the internet. This involves the transaction of goods and services, the transfer of funds and the exchange of data.</p>
+      <p>
+      E-commerce is a popular term for electronic commerce or even internet commerce. The name is self-explanatory, it is the meeting of buyers and sellers on the internet. This involves the transaction of goods and services, the transfer of funds and the exchange of data.
+      </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
---
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useContext } from 'react'
+import { ShopContext} from  '../../Context/ShopContext';
+import './RelatedProducts.css'
+import Items from '../Items/Items';
+// import data_product  from '../Assets/Assets/Frontend_Assets/data';
+
+const RelatedProducts = (props) => {
+    const {all_product} =  useContext(ShopContext);
+    console.log(props.product.category)
+    const related_products = all_product.filter((item)=>item.category === props.product.category)
+    console.log('..',related_products)
+  
+    return (
+    <div className='relatedproducts'>
+        <h1>Related Products</h1>
+        <hr />
+        <div className="relatedproducts-items">
+            {related_products.map((item,i)=>{
+                return <Items key ={i} id={item.id} name= {item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            })}
+        </div>
+      
+    </div>
+  )
+}
+
+export default RelatedProducts
